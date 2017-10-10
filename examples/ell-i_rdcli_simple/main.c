3f44bdbb5e74e127d30acdc7bc8e8fef9c366899
Pekka Nikander <pekka.nikander@iki.fi>
2017-10-10 13:54:42 +0300

    Berlin ACM ICN version of examples/ell-i_rdcli_simple/main.c

--- examples/ell-i_rdcli_simple/main.c
+++ examples/ell-i_rdcli_simple/main.c
@@ -24,61 +24,66 @@
 #include "net/gcoap.h"
 #include "net/rdcli_common.h"
 
+#include "xtimer.h"
+#include "periph/adc.h"
+#include "luid.h"
+#include "fmt.h"
+
+#include "coap_handler.h"
+#include "coap_observer.h"
+
 #define BUFSIZE             (64U)
 
 static char riot_info[BUFSIZE];
 
-/* define some dummy CoAP resources */
-static ssize_t text_resp(coap_pkt_t *pdu, uint8_t *buf, size_t len,
-                         const char *text, unsigned format)
-{
-    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-    size_t slen = strlen(text);
-    memcpy(pdu->payload, text, slen);
-    return gcoap_finish(pdu, slen, format);
-}
-
-static ssize_t handler_info(coap_pkt_t *pdu, uint8_t *buf, size_t len)
-{
-    return text_resp(pdu, buf, len, riot_info, COAP_FORMAT_JSON);
-}
-
-static ssize_t handler_foo(coap_pkt_t *pdu, uint8_t *buf, size_t len)
-{
-    return text_resp(pdu, buf, len, "foo", COAP_FORMAT_TEXT);
-}
-
-static ssize_t handler_bar(coap_pkt_t *pdu, uint8_t *buf, size_t len)
-{
-    return text_resp(pdu, buf, len, "bar", COAP_FORMAT_TEXT);
-}
-
-static const coap_resource_t resources[] = {
-    { "/riot/bar",  COAP_GET, handler_bar },
-    { "/riot/foo",  COAP_GET, handler_foo },
-    { "/riot/info", COAP_GET, handler_info }
-};
-
-static gcoap_listener_t listener = {
-    .resources     = (coap_resource_t *)&resources[0],
-    .resources_len = sizeof(resources) / sizeof(resources[0]),
-    .next          = NULL
-};
-
 int main(void)
 {
-    puts("CoAP simplified RD registration example!\n");
+    puts("RIOT Ell-i gcoap application");
+    puts("based on CoAP simplified RD registration example!\n");
+
+    /* Initialise Arduino digital I/O pins, input by default */
+    /* Do NOT initialise D0: shell thread will block on a mutex for unknown reason */
+    /* Do NOT initialise D1: reason unknown */
+    gpio_init(ARDUINO_PIN_2,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_3,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_4,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_5,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_6,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_7,  GPIO_OUT);
+    gpio_init(ARDUINO_PIN_8,  GPIO_IN);
+    gpio_init(ARDUINO_PIN_10, GPIO_IN);
+    /* Do NOT initialise D11-D13, used for ENC28J60 SPI */
+
+    /* Initialise Arduino analog I/O pins */
+    adc_init(ADC_LINE(0));
+    adc_init(ADC_LINE(1));
+    adc_init(ADC_LINE(2));
+    adc_init(ADC_LINE(3));
+    adc_init(ADC_LINE(4));
+    adc_init(ADC_LINE(5));
 
     /* fill riot info */
-    sprintf(riot_info, "{\"ep\":\"%s\",\"lt\":%u}",
-            rdcli_common_get_ep(), RDCLI_LT);
+    sprintf(riot_info, "{\"lt\":%u}", RDCLI_LT);
 
-    /* register resource handlers with gcoap */
-    gcoap_register_listener(&listener);
+    puts("Waiting for address autoconfiguration...");
+    xtimer_sleep(3);
+
+    /* print network addresses */
+    extern int _netif_config(int argc, char **argv); // XXX Not in any headers ....
+    puts("Configured network interfaces:");
+    _netif_config(0, NULL);
+
+    /* Register the CoAP resource handlers */
+    gcoap_register_handlers();
+
+    /* Start CoAP observer thread */
+    (void)gcoap_observe_init();
+
+    /* Start the coap listener thread */
+    (void)gcoap_init();
 
     /* print RD client information */
     puts("Client information:");
-    printf("  ep: %s\n", rdcli_common_get_ep());
     printf("  lt: %is\n", (int)RDCLI_LT);
 
     /* run the shell */
