9522394b0764a79e7164d0b49bcf297079de0e09
Pekka Nikander <pekka.nikander@iki.fi>
2018-01-06 09:24:02 +0200

    [https://trello.com/c/PgK7I7Cm] Fix sending observer replies

--- examples/ell-i_rdcli_simple/coap_functions.c
+++ examples/ell-i_rdcli_simple/coap_functions.c
@@ -19,12 +19,25 @@
 #include "coap_functions.h"
 #include "coap_observer.h"
 
+ssize_t gcoap_reply_simple(
+    coap_pkt_t *pkt,
+    unsigned code,
+    uint8_t *buf, size_t len,
+    unsigned ct,
+    const uint8_t *payload,
+    uint8_t payload_len) {
+
+    gcoap_resp_init(pkt, buf, len, code);
+    memcpy(pkt->payload, payload, payload_len);
+    return gcoap_finish(pkt, payload_len, ct);
+}
+
 ssize_t coap_arduino_digital_get(
     coap_pkt_t *pkt, uint8_t *buf, size_t len, elli_coap_resource_t *res) {
 
     uint32_t gpio_pin = res->item;
 
-    return coap_reply_simple(
+    return gcoap_reply_simple(
 	pkt, COAP_CODE_CONTENT, buf, len,
 	COAP_FORMAT_TEXT, (unsigned char *) (gpio_read(gpio_pin)? "1": "0"), 2);
 }
@@ -53,7 +66,7 @@ ssize_t coap_arduino_digital_put(
 	DEBUG("CoAP PUT: pin %lu clear\n", gpio_pin);
         gpio_clear(gpio_pin);
     }
-    return coap_reply_simple(
+    return gcoap_reply_simple(
 	pkt, COAP_CODE_CHANGED, buf, len,
 	COAP_FORMAT_NONE, NULL, 0);
 }
@@ -89,7 +102,7 @@ ssize_t coap_arduino_analog_get(
     digits[3] = (sample /    1) % 10 + '0';
     digits[4] = '\0';
 
-    return coap_reply_simple(
+    return gcoap_reply_simple(
 	pkt, COAP_CODE_CONTENT, buf, len,
 	COAP_FORMAT_TEXT, (uint8_t*)digits, sizeof(digits)-1);
 }
