81057caa3e4277b30a734306d3dcfe502223af3d
Pekka Nikander <pekka.nikander@iki.fi>
2018-01-10 14:32:04 +0200

    Add copyright notice to coap_observer.c

--- examples/ell-i_rdcli_simple/coap_observer.c
+++ examples/ell-i_rdcli_simple/coap_observer.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2017 Ell-i open source co-operative
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
 #include <stdlib.h>
 #include "net/gcoap.h"
